b83b26961de52087ff999a7c05028c6c0c3330dc
ahn-sj <jaesa5221@gmail.com>
2024-11-14 00:35:52 +0900

    step2: 계산기, 표현, 연산자 단위 테스트 추가

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -3,10 +3,24 @@ package calculator
 import java.util.function.BiFunction
 
 enum class Operator(val operation: BiFunction<Double, Double, Double>) {
-    ADD(BiFunction { left, right -> left + right }),
-    SUBTRACT(BiFunction { left, right -> left - right }),
-    MULTIPLY(BiFunction { left, right -> left * right }),
-    DIVIDE(BiFunction { left, right -> left / right }), ;
+    ADD(
+        BiFunction { left, right -> left + right },
+    ),
+    SUBTRACT(
+        BiFunction { left, right -> left - right },
+    ),
+    MULTIPLY(
+        BiFunction { left, right -> left * right },
+    ),
+    DIVIDE(
+        BiFunction { left, right ->
+            if (right == 0.0) {
+                throw IllegalArgumentException("0으로 나눌 수 없습니다")
+            } else {
+                left / right
+            }
+        },
+    ), ;
 
     fun apply(
         left: Double,
