7e1f1f2a2a0e605906e425674b2412c58d414c3c
ahn-sj <jaesa5221@gmail.com>
2024-11-17 20:12:34 +0900

    [STEP3] refactor: output 메시지를 응집, 의미를 갖는 클래스 이름으로 변경

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -2,8 +2,8 @@ package racingcar
 
 import racingcar.view.CarInputView
 import racingcar.view.CarOutputView
-import racingcar.view.DisplayConstant.Companion.CAR_VIEW_MESSAGE
-import racingcar.view.DisplayConstant.Companion.TRY_VIEW_MESSAGE
+import racingcar.view.GameGuideMessage.Companion.CAR_VIEW_MESSAGE
+import racingcar.view.GameGuideMessage.Companion.TRY_VIEW_MESSAGE
 
 class RacingGame(
     private val inputView: CarInputView,
